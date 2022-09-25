cfb971fc4f2143c8971bc126f2acca979d07696e
jduchit <johanadbt@gmail.com>
2022-09-25 15:31:17 -0500

    ejercicio 3 mejora ejercicio 2 formula de Gauss

---
+++ Class_exercises/class_exercise_2(sum-of-numbers-improved).py
@@ -0,0 +1,13 @@
+###          ESTRUCTURA DE DATOS          ###
+### Nombre: Johana Duchi Tipan            ###
+### Fecha : 30/08/2022                    ###
+### Tema  : Control de versiones Git      ###
+### Ejercicio 3: Mejora ejercicio 2       ###
+
+#Repeat the exercise using a more efficient way     #
+#Count the number of operations this function takes #
+
+def suma (n):
+    return n * (n+1) / 2 
+
+print (suma(10))
