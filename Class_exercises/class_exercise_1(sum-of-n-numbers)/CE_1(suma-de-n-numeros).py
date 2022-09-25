d18e8c67ab29c891604a1cf1af8a09bf9356841a
jduchit <johanadbt@gmail.com>
2022-09-25 15:01:49 -0500

    ejercicio 2 suma de n numeros

---
+++ Class_exercises/class_exercise_1(sum-of-n-numbers)/CE_1(suma-de-n-numeros).py
@@ -0,0 +1,17 @@
+###          ESTRUCTURA DE DATOS          ###
+### Nombre: Johana Duchi Tipan            ###
+### Codigo: 00321980                      ###
+### Fecha : 30/08/2022                    ###
+### Tema  : Control de versiones Git      ###
+### Ejercicio 2: Practicar con Git        ###
+
+#Write a function that takes calculates the sum of numbers from 1 to n, where n is the input#
+#Escribe una funcion que calcule la suma de numeros de 1 a n, donde n es la entrada#
+
+def suma (n):
+    result = 0
+    for suma in range(1, n+1):
+        result = result + suma
+    print("La suma de numeros de 1 a {} es: {}".format(n,result))
+
+suma(10)                           
